3652232d40aeb83a44259ca11ee5651128762722
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-13 15:54:30 -0600

    Updates food request spec, adds more data to test and updates test, all is green

--- spec/requests/api/v1/food_requests_spec.rb
+++ spec/requests/api/v1/food_requests_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Food Requests' do
+
+  before :each do
+    @food1 = Food.create!(name: 'Banana', calories: 150)
+    @food2 = Food.create!(name: 'Pumpkin Pie', calories: 800)
+  end
+
   describe 'GET /api/v1/foods' do
     it 'should return all of the foods currently in the database' do
-      food1 = Food.create!(name: 'Banana', calories: 150)
 
       get '/api/v1/foods'
 
       expect(response).to be_successful
-      expect(response.body).to eq([food1].to_json)
+      expect(response.body).to eq([@food1, @food2].to_json)
     end
   end
 
   describe 'GET /api/v1/food/:id' do
     it 'should return a single food if found' do
-      
+
     end
 
     it 'should return 404 if not found' do
