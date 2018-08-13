8725c36a788b1c7fbaa5173615ed8614599bafd3
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-13 15:41:20 -0600

    Tests get route for /api/v1/foods to foods#index

---
+++ spec/requests/api/v1/food_requests_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Food Requests' do
+  describe 'GET /api/v1/foods' do
+    it 'should return all of the foods currently in the database' do
+      food1 = Food.create!(name: 'Banana', calories: 150)
+
+      get '/api/v1/foods'
+
+      expect(response).to be_successful
+      expect(response.body).to eq(food1.to_json)
+    end
+  end
+end
