17bb1376df8c2442e1f9ab4062719e9d06b8b0cd
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-14 14:37:54 -0600

    Tests meal#show action endpoint for showing all foods associated with a specified meal

--- spec/requests/api/v1/meal_requests_spec.rb
+++ spec/requests/api/v1/meal_requests_spec.rb
@@ -32,4 +32,26 @@ RSpec.describe 'Meal Requests' do
       expect(response.body).to eq(expected.to_json) 
     end
   end
+
+  describe 'GET /api/v1/meals/:meal_id/foods' do
+    it 'should return all foods associated with the meal specified by the give meal_id' do
+      expected = {
+        id: @meal2.id,
+        name: @meal2.name,
+        foods: [@food3]
+      }
+
+      get "/api/v1/meals/#{@meal2.id}/foods"
+
+      expect(response).to be_successful
+      expect(response.body).to eq(expected.to_json)
+    end
+
+    it 'should return a 404 with an error message if the specified meal is not found' do
+      get "/api/v1/meals/#{@meal2.id + 1}"
+
+      expect(response).to have_http_status(404)
+      expect(response.body).to eq({ error: "Meal not found" }.to_json)
+    end
+  end
 end
