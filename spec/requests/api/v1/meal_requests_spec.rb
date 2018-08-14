2ccfc1e5e27695bb3b18eb494f8ef82bc0df3397
Jude Lawson <lawsonjdutton@gmail.com>
2018-08-14 14:06:59 -0600

    Tests mels#index action endpoint

---
+++ spec/requests/api/v1/meal_requests_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Meal Requests' do
+
+  before :each do
+    @meal1 = Meal.create!(name: 'Breakfast')
+    @food1 = @meal1.foods.create!(name: 'Cheerios', calories: 300)
+    @food2 = @meal1.foods.create!(name: 'Strawberries', calories: 200)
+
+    @meal2 = Meal.create!(name: 'Lunch')
+    @food3 = @meal2.foods.create!(name: 'Ham Sandwich', calories: 800)
+  end
+  
+  describe 'GET /api/v1/meals' do
+    it 'should return all of the meals in the database' do
+      expected = [
+        {
+          id: @meal1.id,
+          name: @meal1.name,
+          foods: [@food1, @food2]
+        },
+        {
+          id: @meal2.id,
+          name: @meal2.name,
+          foods: [@food3]
+        }
+      ]
+
+      get '/api/v1/meals'
+
+      expect(response).to be_successful
+      expect(response.body).to eq(expected.to_json) 
+    end
+  end
+end
